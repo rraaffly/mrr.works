d65b0c51f0b9b2e04f25e21559d0669d81d46e62
raaafly <muh.raflyrafsanjani@gmail.com>
2022-12-29 07:08:15 +0700

    data navigation menu

---
+++ src/data/dataNavigation.ts
@@ -0,0 +1,22 @@
+export const dataNavigationMenu = [
+  {
+    menu: "home",
+    path: "#",
+  },
+  {
+    menu: "about",
+    path: "#",
+  },
+  {
+    menu: "works",
+    path: "#",
+  },
+  {
+    menu: "posts",
+    path: "#",
+  },
+  {
+    menu: "etc.",
+    path: "#",
+  },
+];
