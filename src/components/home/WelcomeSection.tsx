ef6424b2a8628b396341808682c478a96c88b45e
raaafly <muh.raflyrafsanjani@gmail.com>
2022-12-29 07:08:56 +0700

    add home component

---
+++ src/components/home/WelcomeSection.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "../ui";
+
+const WelcomeSection = () => {
+  return (
+    <section id="welcome" className="py-32">
+      <Container>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus,
+          accusantium fugit. Rem eum veniam atque qui maxime, possimus fugiat.
+          Possimus incidunt sequi quia nulla commodi quas quo veritatis iusto
+          ea?
+        </p>
+      </Container>
+    </section>
+  );
+};
+
+export default WelcomeSection;
