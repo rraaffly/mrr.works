0cbf61251793cbf3970fb0b5779a6c88872e60be
raaafly <muh.raflyrafsanjani@gmail.com>
2022-12-29 07:08:05 +0700

    add layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,12 @@ import "~/styles/globals.css";
 
 import React from "react";
 import type { AppProps } from "next/app";
+import { Layout } from "~/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
