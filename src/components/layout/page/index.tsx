4cd7db067e1defb20cb6446fc82c1bfbd51a698c
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-02 09:12:36 +0700

    change typescript to javascript

--- src/components/layout/page/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as PageContent } from "./PageContent";
-export { default as PageFooter } from "./PageFooter";
-export { default as PageHeader } from "./PageHeader";
-export { default as PageLayout } from "./PageLayout";
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,9 @@
 import "~/styles/globals.css";
 
 import React from "react";
-import type { AppProps } from "next/app";
 import { Layout } from "~/components/layout";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
