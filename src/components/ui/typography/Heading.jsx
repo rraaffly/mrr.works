2177ac36e2b1e69f450ff549204a7dafb186eb25
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-06 16:29:27 +0700

    add default styling Heading component

--- src/components/ui/typography/Heading.jsx
+++ src/components/ui/typography/Heading.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import clsx from "clsx";
 
-const Heading = ({ children, className, as }) => {
-  return <div>{children}</div>;
+const Heading = ({ text, className, as: HeadingElement, ...props }) => {
+  const isStyled = {
+    h1: "text-6xl",
+    h2: "text-5xl",
+    h3: "text-4xl",
+    h4: "text-3xl",
+    h5: "text-2xl",
+    h6: "text-xl",
+  };
+  return (
+    <HeadingElement
+      className={clsx("", isStyled[HeadingElement], className)}
+      {...props}
+    >
+      {text}
+    </HeadingElement>
+  );
 };
 
 Heading.defaultProps = {
-  as: "",
+  as: "h1",
+  text: "Title",
 };
 
 export default Heading;
