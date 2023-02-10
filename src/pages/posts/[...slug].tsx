a995e05607bc8b988a0dc455160409a1e5f1acdb
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-10 18:53:13 +0700

    dynamic route detail post

---
+++ src/pages/posts/[...slug].tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function PostsDetailPage() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic fugit,
+        minima repellendus porro nobis reprehenderit, numquam, nostrum id
+        impedit odit ullam quia modi ipsum natus consequuntur quaerat vitae
+        facilis tempore?
+      </p>
+    </div>
+  );
+}
