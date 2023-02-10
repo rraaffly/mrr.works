8a25664701615f3358222127922a2a0d66c8cb63
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-10 18:50:46 +0700

    redesign navigation

--- src/components/layout/main/navigation/Navigation.tsx
+++ src/components/layout/main/navigation/Navigation.tsx
@@ -9,24 +9,28 @@ const Navigation = () => {
   const [isOpen, setOpen] = useState(false);
   const handleClick = () => setOpen(!isOpen);
   return (
-    <nav className="flex flex-col lg:flex-row bg-blue-200">
+    <nav className="flex flex-col lg:flex-row">
       <div className="flex items-center justify-between">
-        <div className="w-full lg:w-64">
+        <div className="w-full lg:w-72 h-7 flex items-center justify-start">
           <Link href="/">Logo</Link>
         </div>
         <button
           onClick={handleClick}
-          className="lg:hidden inline-flex items-center justify-center w-7 h-7 text-lg"
+          className={clsx(
+            "lg:hidden inline-flex items-center justify-center w-7 h-7",
+            isOpen ? "absolute right-6 z-50" : "static"
+          )}
         >
           {isOpen ? <X weight="bold" /> : <List weight="bold" />}
         </button>
       </div>
+
       <div
         className={clsx(
-          "absolute top-0 flex w-full transition-all duration-300 ease-in-out md:static lg:justify-end",
+          "absolute top-0 z-40 w-full flex items-center justify-center transition-all duration-300 lg:static lg:h-fit lg:justify-end",
           {
-            "bg-red-500 right-0": isOpen,
-            "bg-blue-500 -right-full": !isOpen,
+            "left-0 h-screen bg-white/30 backdrop-blur-md": isOpen,
+            "left-full h-screen": !isOpen,
           }
         )}
       >
