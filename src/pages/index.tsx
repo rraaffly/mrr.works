f65349d574ffd7faddd107fd25033baeb6ddb585
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-10 18:52:55 +0700

    add index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { MainContent, MainLayout } from "~/components/layout";
+import { Container, Heading } from "~/components/ui";
 
 export default function HomePage() {
   return (
-    <h1>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repudiandae
-      corrupti est facilis inventore, aperiam dicta error, accusamus possimus
-      magnam id, laudantium omnis doloremque voluptates. Ullam non temporibus
-      ipsa itaque.
-    </h1>
+    <MainLayout pageTitle="Home">
+      <MainContent>
+        <section id="home" className="py-32 my-auto">
+          <Container>
+            <Heading as="h6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+              quo.
+            </Heading>
+            <Heading as="h6">
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </Heading>
+          </Container>
+        </section>
+      </MainContent>
+    </MainLayout>
   );
 }
