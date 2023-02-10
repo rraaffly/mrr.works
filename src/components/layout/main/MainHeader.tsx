01e53373c3b940f2f3608af29ae1d8bde698adf4
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-10 18:50:34 +0700

    add styling in header

--- src/components/layout/main/MainHeader.tsx
+++ src/components/layout/main/MainHeader.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Container } from "~/components/ui";
 import Navigation from "./navigation/Navigation";
+import { Container } from "~/components/ui";
 
 const MainHeader = () => {
   return (
-    <header className="relative">
-      <Container>
+    <header className="fixed top-0 left-0 w-full">
+      <Container className="py-3">
         <Navigation />
       </Container>
     </header>
