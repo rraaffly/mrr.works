d200e6cfd3943a3265306eaae625ac956be75d00
raaafly <muh.raflyrafsanjani@gmail.com>
2023-02-12 21:49:33 +0700

    add config title page

--- src/components/layout/main/MainLayout.tsx
+++ src/components/layout/main/MainLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DefaultSeo } from "next-seo";
 import MainFooter from "./MainFooter";
 import MainHeader from "./MainHeader";
-import seoConfig from "~/config/website.config.json";
+import seoConfig from "~/config/seo.config.json";
 
 const metaConfig = {
   metaSiteName: seoConfig.name,
@@ -20,7 +20,7 @@ const MainLayout = ({ children, pageTitle }: PageProps) => {
   return (
     <>
       <DefaultSeo
-        title={`${metaConfig.metaTitle} - ${pageTitle}`}
+        title={`${pageTitle} - ${metaConfig.metaTitle}`}
         description={metaConfig.metaDescription}
         openGraph={{
           locale: "id_ID",
