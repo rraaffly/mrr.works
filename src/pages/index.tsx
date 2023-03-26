4f244e35fa2d9875449e0494596b3bfae0371b60
raaafly <muh.raflyrafsanjani@gmail.com>
2023-03-26 21:42:23 +0700

    page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,13 @@
 import React from "react";
-import { IoDownloadOutline } from "react-icons/io5";
-import { MainContent, MainLayout } from "~/components/layout";
-import { ButtonLink, Container, Heading, Link, Text } from "~/components/ui";
+import { JumbotronSection } from "~/components/jumbotron";
+import { PageContent, PageLayout } from "~/components/layout";
 
-export default function HomePage() {
+export default function HomeIndex() {
   return (
-    <MainLayout pageTitle="Home">
-      <MainContent>
-        <section id="home" className="my-auto">
-          <Container className="flex flex-col items-center lg:items-start">
-            <Text as="span">Hello, I&apos;m</Text>
-            <Heading as="h1" bold className="tracking-tight uppercase text-lg">
-              Muhammad Rafly Rafsanjani
-            </Heading>
-            <Heading as="h4">Software Engineer</Heading>
-            <Text as="span" className="block mt-10 text-lg">
-              🚀 Exploring opportunities and side projects.
-            </Text>
-            <Text as="span" className="block text-lg">
-              🚀 Currently specializing in Front-end Development.
-            </Text>
-            <ButtonLink size="md" href="/" className="mt-4">
-              Download Resume <IoDownloadOutline className="ml-2.5 text-lg" />
-            </ButtonLink>
-          </Container>
-        </section>
-      </MainContent>
-    </MainLayout>
+    <PageLayout>
+      <PageContent>
+        <JumbotronSection />
+      </PageContent>
+    </PageLayout>
   );
 }
