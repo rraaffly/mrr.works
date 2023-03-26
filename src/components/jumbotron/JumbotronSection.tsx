243639b6d72aa9cc01faa103e2e8652342fac8cc
raaafly <muh.raflyrafsanjani@gmail.com>
2023-03-26 21:41:41 +0700

    jumbotron section component

---
+++ src/components/jumbotron/JumbotronSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "../ui";
+
+const JumbotronSection = () => {
+  return (
+    <section id="jumbotron">
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+          iusto at? Sit ad magnam nobis, officia omnis ut debitis reiciendis sed
+          distinctio. At culpa ipsam dolore numquam id molestias dolorem.
+        </p>
+      </Container>
+    </section>
+  );
+};
+
+export default JumbotronSection;
---
+++ src/components/jumbotron/index.ts
@@ -0,0 +1,1 @@
+export { default as JumbotronSection } from "./JumbotronSection";
